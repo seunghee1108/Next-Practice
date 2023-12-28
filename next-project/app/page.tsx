20362a925d4194f67577b25895591c75a0c938bc
승희 <bangseunghee1108@gmail.com>
2023-12-29 02:58:27 +0900

    style 작성하는 방법은 여러가지가 있음

--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <div className="login">로그인</div>
+      <div style={{ color:'blue', fontSize:'20px'}}>로그인</div>
       <div className="menu" >메뉴</div>
       <div className="product">상품</div>
     </div>
