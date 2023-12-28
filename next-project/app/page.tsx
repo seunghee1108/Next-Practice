b1c943f82c3d1244daef06c6c65c64c23b09bdab
승희 <bangseunghee1108@gmail.com>
2023-12-29 03:26:59 +0900

    메인 페이지 내용 수정

--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -1,11 +1,9 @@
+import Link from "next/link";
 
-export default function Home() {
+export default function Home () {
   return (
     <div>
-      <div style={{ color:'blue', fontSize:'20px'}}>로그인</div>
-      <div className="menu" >메뉴</div>
-      <div className="product">상품</div>
+      <h2>MainPage</h2>
     </div>
   )
 }
- 
