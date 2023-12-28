8c009f737029a73bd4b53479f7ab5ccd0b510d9f
승희 <bangseunghee1108@gmail.com>
2023-12-29 03:28:13 +0900

    로그인, 메뉴, 상품 페이지 내용 작성

---
+++ next-project/app/menu/page.tsx
@@ -0,0 +1,7 @@
+export default  function List (){
+  return (
+    <div>
+      <h2>메뉴 페이지임</h2>
+    </div>
+  )
+}
---
+++ next-project/app/product/page.tsx
@@ -0,0 +1,7 @@
+export default  function List (){
+  return (
+    <div>
+      <h2>상품 페이지임</h2>
+    </div>
+  )
+}
