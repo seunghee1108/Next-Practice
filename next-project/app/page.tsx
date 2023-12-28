3bd4156a0bfce83331c23d9f4d55d25a1010a81c
승희 <bangseunghee1108@gmail.com>
2023-12-29 02:53:24 +0900

    div 생성

--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <div>
-
+      <div className="login">로그인</div>
+      <div className="menu" >메뉴</div>
+      <div className="product">상품</div>
     </div>
   )
 }
